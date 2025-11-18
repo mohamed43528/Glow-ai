80dc2d7482697bcdca5a69025dc674f357190b87
mohamed43528 <lamarsmohamed696@gmail.com>
2025-11-18 12:42:25 +0000

    Add auth controller with register and login methods

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+// src/controllers/auth.controller.js
+
+export const register = async (req, res) => {
+  return res.json({ message: "Register controller working" });
+};
+
+export const login = async (req, res) => {
+  return res.json({ message: "Login controller working" });
+};
