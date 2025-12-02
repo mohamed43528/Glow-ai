fd2742cfef9f03e9b6daa2922eea4c4b14171107
mohamed43528 <lamarsmohamed696@gmail.com>
2025-12-02 23:01:44 +0000

    Add imports and dotenv configuration to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,3 @@
+import express from "express"; import cors from "cors"; import dotenv from "dotenv"; import bodyParser from "body-parser"; import { PrismaClient } from "@prisma/client";
 
+dotenv.config();
