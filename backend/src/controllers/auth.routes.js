1e16efe031ef5c6762f2bdacb4df3538d6390dad
mohamed43528 <lamarsmohamed696@gmail.com>
2025-12-03 16:58:47 +0000

    Add authentication routes for magic link

---
+++ backend/src/controllers/auth.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { sendMagicLink, verifyMagicLink } from "../controllers/auth.controller.js";
+
+const router = Router();
+
+router.post("/send-magic-link", sendMagicLink);
+router.post("/verify", verifyMagicLink);
+
+export default router;
