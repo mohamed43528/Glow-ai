977f23fd193b69aae98a0875cceb02f52f82a3d6
mohamed43528 <lamarsmohamed696@gmail.com>
2025-12-03 17:18:49 +0000

    Add route exports for auth, customer, and business

---
+++ backend/src/index.js
@@ -0,0 +1,3 @@
+export { default as authRoutes } from "./routes/auth.routes.js";
+export { default as customerRoutes } from "./routes/customer.routes.js";
+export { default as businessRoutes } from "./routes/business.routes.js";
